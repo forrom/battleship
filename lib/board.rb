26ff1f0b2a798ac45f22020142cec62d732f51bd
forrom <forrom@gmail.com>
2018-07-13 21:49:46 +0200

    add vertical ships, overlapping rule, hitting second ship

--- lib/board.rb
+++ lib/board.rb
@@ -4,28 +4,63 @@ class Board
     end
 
     def place(ship, x, y, orientation)
+        planned_location = ship.plan_segments(x, y, orientation)
+        taken_locations = []
+        @ships.each do |placed_ship|
+            taken_locations += placed_ship.initial_segments
+        end
+        unless (planned_location & taken_locations).empty? then
+            return
+        end
         @ships << ship
         ship.place x, y, orientation
     end
 
     def shoot(x,y)
-        @ships[0].hit(x,y)
+        if ship = ship_at(x,y) then 
+            ship.hit(x,y)
+        end
     end
 
+    def ship_at(x,y)
+        @ships.find {|ship| ship.initial_segments.include? [x,y]}
+    end
 end
 
 class Battleship
-    Length = 3
+    Length = 5 
+    
+    def initial_segments
+        @initial_segments
+    end
 
     def initialize ()
         @intact_segments = []
+        @initial_segments = []
+        @placed = false
+    end
+
+    def plan_segments(x, y, orientation)
+        segments = []
+        Length.times do
+            segments << [x,y]
+            if orientation == :horizontal then 
+                x = x + 1
+            elsif orientation == :vertical then
+                y = y + 1
+            end
+        end
+        segments
     end
 
     def place (x, y, orientation)
-        5.times do
-            @intact_segments << [x,y]
-            x = x + 1
-        end
+        @initial_segments = plan_segments(x, y, orientation)
+        @intact_segments = @initial_segments.dup
+        @placed = true
+    end
+
+    def placed?
+        @placed
     end
 
     def sunk?
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,14 @@
 require "./lib/board"
 
-describe 'board' do 
+describe 'place and sink ship' do 
+    it "ship can be placed" do
+      
+        board = Board.new(10,10)
+        battleship = Battleship.new()
+        expect(battleship.placed?).to be false
+        board.place(battleship, 2,3, :horizontal)
+        expect(battleship.placed?).to be true
+    end
     it "ship can be sunk" do
         board = Board.new(10,10)
         battleship = Battleship.new()
@@ -33,5 +41,82 @@ describe 'board' do
         board.shoot(6,3)
 
         expect(battleship.sunk?).to be false
+    end
+    
+    it "ship can be placed vertically then sunk" do
+        board = Board.new(10,10)
+        battleship = Battleship.new()
+        board.place(battleship, 2,3, :vertical)
+        board.shoot(2,3)
+        expect(battleship.sunk?).to be false
+        
+        board.shoot(2,4)
+        expect(battleship.sunk?).to be false
+        
+        board.shoot(2,5)
+        expect(battleship.sunk?).to be false
+        
+        board.shoot(2,6)
+        expect(battleship.sunk?).to be false
+        
+        board.shoot(2,7)
+        expect(battleship.sunk?).to be true
+    end
+
+    it "ships cannot overlap" do
+        board = Board.new(10,10)
+
+        battleship_horizontal = Battleship.new()
+        board.place(battleship_horizontal, 0,3, :horizontal)
+        expect(battleship_horizontal.placed?).to be true
+
+        battleship_vertical = Battleship.new()
+        expect(board.place(battleship_vertical, 2,3, :vertical))
+        expect(battleship_vertical.placed?).to be false
+    end
+    
+    it "ships cannot overlap with second ship" do
+        board = Board.new(10,10)
+        
+        battleship1 = Battleship.new()
+        board.place(battleship1, 0, 0, :horizontal)
+        expect(battleship1.placed?).to be true
+        
+        battleship2 = Battleship.new()
+        board.place(battleship2, 0, 3, :horizontal)
+        expect(battleship2.placed?).to be true
+        
+        battleship3 = Battleship.new()
+        board.place(battleship3, 2, 3, :vertical)
+        expect(battleship3.placed?).to be false
+    end
+
+    it "second ship can be sunk" do
+        #1. Init 10x10 board
+        board = Board.new(10,10)
+        #2. Place battleship size 5 at 0,0 horizontally -> verify ship is placed
+        battleship1 = Battleship.new()
+        board.place(battleship1, 0, 0, :horizontal)
+        expect(battleship1.placed?).to be true
+        #3. Place battleship size 5 at 0,3 horizontally -> verify ship is placed
+        battleship2 = Battleship.new()
+        board.place(battleship2, 0, 3, :horizontal)
+        expect(battleship2.placed?).to be true
+        #4. Shoot
+        board.shoot(0,3)
+        expect(battleship2.sunk?).to be false
+
+        board.shoot(1,3)
+        expect(battleship2.sunk?).to be false
+
+        board.shoot(2,3)
+        expect(battleship2.sunk?).to be false
+        
+        board.shoot(3,3)
+        expect(battleship2.sunk?).to be false
+        
+        board.shoot(4,3)
+        expect(battleship2.sunk?).to be true
+        expect(battleship1.sunk?).to be false
     end
 end
